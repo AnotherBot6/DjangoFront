a333455d8345718bbacd48c450c032b743b5c2f8
Charles M <107741072+AnotherBot6@users.noreply.github.com>
2023-04-12 13:53:47 -0600

    Update settings.py

--- djangofront/settings.py
+++ djangofront/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-$l+^wietw$q+ix#n)==1dkew029d)vx4-^8^t9wfk9y3j0zqnr"
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
